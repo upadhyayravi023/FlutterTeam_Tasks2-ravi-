49cf0e7045cef3f874ffa5b640c3ca7dee77754e
TanyaSingh67 <tanyaplatformideas@gmail.com>
2024-06-25 21:05:01 +0530

    env.file

--- Task2/tanya/tanya_gemini_chatbot/lib/main.dart
+++ Task2/tanya/tanya_gemini_chatbot/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:tanya_gemini_chatbot/homepage.dart';
 
 Future<void> main() async {
-  Gemini.init(apiKey: 'AIzaSyB9sRcQm16DyMg6f2oogQ-oe3-iLKfv_RQ');
+  Gemini.init(apiKey: 'API_key');
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName:".env");
   runApp(MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 100, 167, 243),
+          seedColor: const Color.fromARGB(255, 100, 167, 243),
         ),
         useMaterial3: true,
       ),
