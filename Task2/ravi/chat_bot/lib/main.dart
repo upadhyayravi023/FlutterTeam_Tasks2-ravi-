261d9e32958aeb89283e7418c7dc19c29d304fe0
ivar <upadhyayravi2005@gmail.com>
2024-07-01 12:23:26 +0530

    final commit

---
+++ Task2/ravi/chat_bot/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:chat_bot/page1.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+void main() async{
+  await dotenv.load(fileName: ".env");
+  Gemini.init(apiKey:dotenv.env["API_key"].toString());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat Bot',
+      home: HomePage(),
+    );
+  }
+}
